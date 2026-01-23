89a1b02a6637386c8a9085b4cabb19d70596bdd3
Víctor Eduardo <desarrollador.victor@gmail.com>
2026-01-23 04:18:26 -0600

    chore(playground): use UI instead components

--- src/view/playground.vue
+++ src/view/playground.vue
@@ -1,16 +1,5 @@
-<!-- <script setup lang="ts">
-  import Icon from '@/components/atom/icon';
-  import TAccordion from '@/components/atom/TAccordion.vue';
-  import TButton from '@/components/atom/TButton.vue';
-  import TForm from '@/components/atom/TForm.vue';
-  import THyperlink from '@/components/atom/THyperlink.vue';
-  import TLabel from '@/components/atom/TLabel.vue';
-  import TLayout from '@/components/atom/TLayout.vue';
-  import TSeparator from '@/components/atom/TSeparator.vue';
-  import TBreadCrumb from '@/components/molecule/TBreadCrumb.vue';
-  import TEdit from '@/components/molecule/TEdit.vue';
-  import TMenu from '@/components/organism/TMenu.vue';
-  import TSidebar from '@/components/organism/TSidebar.vue';
+<script setup lang="ts">
+  import { Icon, UI } from '@/ui';
   import { ref } from 'vue';
 
   function toggleDarkMode() {
@@ -30,9 +19,9 @@
 </script>
 
 <template>
-  <TLayout show-header show-primary-sidebar>
+  <UI.Layout show-header show-primary-sidebar>
     <template #TLayoutHeader>
-      <TMenu
+      <UI.Menu
         orientation="horizontal"
         :items="[
           {
@@ -69,11 +58,11 @@
             ],
           },
         ]"
-      ></TMenu>
+      />
     </template>
 
     <template #TLayoutPrimarySidebar>
-      <TSidebar
+      <UI.Sidebar
         :sections="[
           {
             caption: 'Principal',
@@ -126,58 +115,58 @@
     <div class="playground">
       <h1>Playground</h1>
 
-      <TAccordion title="Controles de formulario">
-        <TForm>
-          <TEdit
+      <UI.Accordion title="Controles de formulario">
+        <UI.Form>
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="date"
             type="date"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="datetime-local"
             type="datetime-local"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="email"
             type="email"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="month"
             type="month"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="number"
             type="number"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             name="edit"
             caption="password"
             type="password"
             placeholder="Hello"
             autocomplete="off"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="search"
             type="search"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="tel"
@@ -185,86 +174,86 @@
             inputmode="tel"
             placeholder="Tel"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="text"
             type="text"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="time"
             type="time"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="url"
             type="url"
             placeholder="Hello"
           />
-          <TEdit
+          <UI.Edit
             autocomplete="off"
             name="edit"
             caption="week"
             type="week"
             placeholder="Hello"
           />
-        </TForm>
-      </TAccordion>
+        </UI.Form>
+      </UI.Accordion>
 
-      <TAccordion title="Botones">
-        <TButton caption="Click me" />
-        <TSeparator />
-        <TButton caption="Click me" mode="filled" />
-        <TSeparator />
-        <TButton caption="Click me" mode="secondary" />
-        <TSeparator />
-        <TButton caption="Click me" mode="outlined" />
-        <TSeparator />
-        <TButton caption="Click me" mode="text" />
-        <TSeparator />
-        <TButton :icon="Icon.Search" caption="Click me" mode="icon" />
-        <TSeparator />
-        <TButton :icon="Icon.Search" disabled caption="Click me" />
-        <TSeparator />
-        <TButton
+      <UI.Accordion title="Botones">
+        <UI.Button caption="Click me" />
+        <UI.Separator />
+        <UI.Button caption="Click me" mode="filled" />
+        <UI.Separator />
+        <UI.Button caption="Click me" mode="secondary" />
+        <UI.Separator />
+        <UI.Button caption="Click me" mode="outlined" />
+        <UI.Separator />
+        <UI.Button caption="Click me" mode="text" />
+        <UI.Separator />
+        <UI.Button :icon="Icon.Search" caption="Click me" mode="icon" />
+        <UI.Separator />
+        <UI.Button :icon="Icon.Search" disabled caption="Click me" />
+        <UI.Separator />
+        <UI.Button
           :icon="Icon.Search"
           disabled
           caption="Click me"
           mode="filled"
         />
-        <TSeparator />
-        <TButton
+        <UI.Separator />
+        <UI.Button
           :icon="Icon.Search"
           disabled
           caption="Click me"
           mode="secondary"
         />
-        <TSeparator />
-        <TButton
+        <UI.Separator />
+        <UI.Button
           :icon="Icon.Search"
           disabled
           caption="Click me"
           mode="outlined"
         />
-        <TSeparator />
-        <TButton :icon="Icon.Search" disabled caption="Click me" mode="text" />
-        <TSeparator />
-        <TButton :icon="Icon.Search" disabled mode="text" />
-        <TSeparator />
-        <TButton :icon="Icon.Search" disabled caption="Click me" mode="icon" />
-        <TSeparator />
-      </TAccordion>
+        <UI.Separator />
+        <UI.Button :icon="Icon.Search" disabled caption="Click me" mode="text" />
+        <UI.Separator />
+        <UI.Button :icon="Icon.Search" disabled mode="text" />
+        <UI.Separator />
+        <UI.Button :icon="Icon.Search" disabled caption="Click me" mode="icon" />
+        <UI.Separator />
+      </UI.Accordion>
 
-      <TAccordion title="Navegación">
-        <THyperlink>Soy un enlace</THyperlink>
-        <THyperlink route="playground">Playground</THyperlink>
-        <THyperlink route="home">Home</THyperlink>
-        <TBreadCrumb
+      <UI.Accordion title="Navegación">
+        <UI.Hyperlink>Soy un enlace</UI.Hyperlink>
+        <UI.Hyperlink route="playground">Playground</UI.Hyperlink>
+        <UI.Hyperlink route="home">Home</UI.Hyperlink>
+        <UI.BreadCrumb
           current="Home"
           :routes="[
             {
@@ -273,14 +262,16 @@
             },
           ]"
         />
-      </TAccordion>
+      </UI.Accordion>
     </div>
-  </TLayout>
+  </UI.Layout>
 </template>
 
 <style scoped>
   .playground {
     padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
   }
-</style> -->
-<template></template>
+</style>
