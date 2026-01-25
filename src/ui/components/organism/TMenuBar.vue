9cf3ecfdef335d00fba7e6adacd35511dd2494e9
Víctor Eduardo <desarrollador.victor@gmail.com>
2026-01-25 05:25:43 -0600

    feat(component): add TMenuBar

---
+++ src/ui/components/organism/TMenuBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import type { IMenu } from '@/types/component.types';
+  import TMenu from '../molecule/TMenu.vue';
+
+  const props = defineProps<IMenu['props']>();
+</script>
+
+<template>
+  <nav class="menu-bar">
+    <TMenu v-bind="props" class="menu-bar__list" />
+  </nav>
+</template>
+
+<style scoped>
+  .menu-bar {
+    padding: 0.125rem;
+    background: var(--menu-background-color);
+    border-bottom: 0.0125rem solid var(--menu-border-color);
+  }
+  .menu-bar__list {
+    border: none;
+  }
+</style>
