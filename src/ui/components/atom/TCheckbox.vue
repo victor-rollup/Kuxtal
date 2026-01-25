fc06c90d655f2b11360b59ca9e197b2c766a0c6a
Víctor Eduardo <desarrollador.victor@gmail.com>
2026-01-25 05:24:26 -0600

    feat(component): add TCheckbox

---
+++ src/ui/components/atom/TCheckbox.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import type { ICheckbox } from '@/types/component.types';
+
+  const model = defineModel<boolean>();
+  const props = defineProps<ICheckbox['props']>();
+</script>
+
+<template>
+  <input type="checkbox" v-model="model" v-bind="props" class="checkbox" />
+</template>
