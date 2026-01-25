60933c7a4f4391f3fad4844b7a47de1dc4da0d85
Víctor Eduardo <desarrollador.victor@gmail.com>
2026-01-25 05:24:34 -0600

    feat(component): add TRadioButton

---
+++ src/ui/components/atom/TRadioButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import type { IRadioButton } from '@/types/component.types';
+
+  const model = defineModel<string | number>();
+  const props = defineProps<IRadioButton['props']>();
+</script>
+
+<template>
+  <input type="radio" v-model="model" v-bind="props" class="radio-button" />
+</template>
