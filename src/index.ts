ad7784d5fffd5d61c4d01e76ac0895b6e6eb1a9b
victor-rollup <desarrollador.victor@gmail.com>
2026-03-07 16:03:45 -0600

    feat(router): change router name

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,7 @@ import('./ui/stylesheet/index.css');
 export * as Schema from './types/schema.types';
 export * as Component from './types/component.types';
 export * as DOMType from './types/dom.types';
-export {
-  createRouter,
-  createWebHistory,
-  type Router,
-  type RouterOptions,
-} from 'vue-router';
+export * as Web from 'vue-router';
 // ------------------------------------------------------------------------------------------------
 export { Icon, Theme, UI } from '@/ui';
 // ------------------------------------------------------------------------------------------------
